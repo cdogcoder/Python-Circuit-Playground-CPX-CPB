8c5d790a3e31af93839fad56b7b13008989d467c
cdogcoder <cdogcoder@gmail.com>
2025-01-23 07:58:55 -0800

    Create W3S-variables-output.py

---
+++ W3S-variables-output.py
@@ -0,0 +1,20 @@
+kevin = "Kevin is awesome sauce"
+
+kev = "kevin"
+kevster = "kevin"
+kevin = "kevin"
+
+print(kev, kevster, kevin)
+
+kev = "kevin "
+kevster = "kevin "
+kevin = "kevin "
+
+print(kev + kevster + kevin)
+
+five_kevins = 5
+sixty_six_kevins = 66
+print(five_kevins + sixty_six_kevins)
+
+just_one_kevin = "kevin"
+print(five_kevins + just_one_kevin)
