2df6009feb55dff1d29e409d1dd3992e413afdeb
cdogcoder <cdogcoder@gmail.com>
2025-01-23 07:53:44 -0800

    Create W3S-variables-multi-values.py

---
+++ W3S-variables-multi-values.py
@@ -0,0 +1,9 @@
+kev, kevster, kevin = "kev", "kevster", "kevin"
+print(kev, kevster, kevin)
+
+kev = kevster = kevin = "kev"
+print(kev, kevster, kevin)
+
+kevins = ["kev", "kevster", "kevin"]
+kev, kevster, kevin = kevins
+print(kev, kevster, kevin)
