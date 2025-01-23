d02fd29250c517e1775d30b3429f29e452bf0d2e
cdogcoder <cdogcoder@gmail.com>
2025-01-23 08:32:13 -0800

    Create Flow-Control-Guess-1.py

---
+++ Flow-Control-Guess-1.py
@@ -0,0 +1,14 @@
+print('This is a Guessing Game')
+print("Ready to play?")
+
+num2_guess = 120
+user_guess = 123
+
+print(num2_guess)
+print(type(num2_guess))
+print(user_guess)
+
+if num2_guess == user_guess:
+  print("Good Guess, you are a Winner")
+else:
+  print("BETTER LUCK NEXT TIME BUD")
