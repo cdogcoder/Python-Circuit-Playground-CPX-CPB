dd1abcdffaa2f405b629b1c048f95f7c8bcec70f
cdogcoder <cdogcoder@gmail.com>
2025-01-23 08:12:55 -0800

    Create Python-Flow-Control-IFELSE.py

---
+++ Python-Flow-Control-IFELSE.py
@@ -0,0 +1,27 @@
+print(720303 == 720303)
+print(6.99999999999 == 7)
+
+print(type(50))
+print(type(3243))
+print(type(True))
+print(type(False))
+
+kevin = "kevin"
+if kevin == "kevin":
+  print("I LOVE KEVIN")
+
+x = 18 
+y = 20
+if x >= y:
+  print(x, "is greater than or equal to", y)
+
+num1 = input("Enter a number: ")
+num1 = int(num1)
+if num1 < num2:
+  print(num1, "is less than", num2)
+elif num1 == num2:
+  print(num1, "is equal to", num2)
+else: 
+  print(num1, "is greater than", num2)
+
+print("Done")
