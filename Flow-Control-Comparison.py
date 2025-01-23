6fe04bcf064c47090febe6173cfe89a6e9678703
cdogcoder <cdogcoder@gmail.com>
2025-01-23 08:27:38 -0800

    Create Flow-Control-Comparison.py

---
+++ Flow-Control-Comparison.py
@@ -0,0 +1,11 @@
+x = 6.9999999999999999
+y = 7
+
+x == y      # False
+x != y      # True
+x > y       # False
+x < y       # True
+x >= y      # False
+x <= y      # True
+x is y      # False
+x is not y  # True
