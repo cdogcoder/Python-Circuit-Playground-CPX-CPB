08a6d5c4310c2a9896ca557dfc9ac4ddf28563ff
cdogcoder <cdogcoder@gmail.com>
2025-01-23 08:19:30 -0800

    Create W3S-IFELIFELSE.py

---
+++ W3S-IFELIFELSE.py
@@ -0,0 +1,45 @@
+kev = 23432
+kevster = 34032332
+if kevster < kev:
+  print("muhahaha")
+
+kev = 23432
+kevster = 34032332
+if kevster > kev:
+print("muhahaha")
+
+kev = 23432
+kevster = 23432
+if kevster > kev:
+  print("muhahaha")
+elif kevster == kev:
+  print("hahahhahauM")
+
+kev = 4321983214
+kevster = 23432
+if kevster > kev:
+  print("muhahaha")
+elif kevster == kev:
+  print("hahahhahauM")
+else:
+  print("LJFSDLJFLKJW")
+
+if True == True: print("Awesome Sauce")
+
+a = 100
+b = 10
+print("hi") if a>b else print("hello")
+
+a = 10
+b = 10
+print("hi") if a>b else print("hello") if a == b else print("hey")
+
+a = 232
+b = 32423
+c = -1
+
+if not (a<=c or b>c and a):
+  print("This is a true condition")
+
+if True:
+  pass
