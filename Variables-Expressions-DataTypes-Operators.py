f33091a2db68f99384af3c2f141baa442f35b2bf
cdogcoder <cdogcoder@gmail.com>
2025-01-28 07:47:48 -0800

    Create Variables-Expressions-DataTypes-Operators.py

---
+++ Variables-Expressions-DataTypes-Operators.py
@@ -0,0 +1,18 @@
+kevin = "kevin"
+kevin = "robert"
+print(kevin)
+
+KEvin = "howard"
+kevin = "Howard"
+print(KEvin == kevin)
+
+x = 123
+y = 21
+
+print(x//y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**2)
