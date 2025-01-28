2ee72af13c9e066676d36f18eada20cb8c6564f7
cdogcoder <cdogcoder@gmail.com>
2025-01-28 08:06:53 -0800

    Create List-1.py

---
+++ List-1.py
@@ -0,0 +1,12 @@
+firstList = ["kevin", "KEVIN", "kevster"]
+print(firstList)
+
+secondList = ["kev", 323, True, 3.21]
+for i in secondList:
+  print(i)
+
+for i in range(len(firstList)):
+  print(firstList[i])
+
+print(type(firstList))
+print(type(secondList))
