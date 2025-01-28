80bbb07be65c37ffde1127d706893c768f342ae2
cdogcoder <cdogcoder@gmail.com>
2025-01-28 08:03:03 -0800

    Update Try-Except-guess-3.py

--- Try-Except-guess-3.py
+++ Try-Except-guess-3.py
@@ -2,6 +2,10 @@ print('This is a Guessing Game')
 print("Ready to play?")
 
 num2_guess = 120
+
+# try block will try to convert the user input into an integer
+# if an error occurs in the try block, the except block will print out a message saying that the user's input was not a number
+
 try:
   user_guess = int(input("Enter a number: "))
   print("Thanks for guessing with the number", user_guess)
