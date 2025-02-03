308b8f994a50db01af740f0c01ea826ba237864a
cdogcoder <cdogcoder@gmail.com>
2025-02-03 08:14:44 -0800

    Create While-Loop-code.py

---
+++ While-Loop-code.py
@@ -0,0 +1,10 @@
+kevins = 10000
+while kevins < 100000:
+  print("I love you Kevin")
+  if kevins == 69000:
+    break
+  elif kevins == 69001:
+    continue
+  kevins += 100
+else:
+  print("ULTIMATE KEVIN")
