83e2e653f83751a0e7ee36f17980c5d6dfae4804
cdogcoder <cdogcoder@gmail.com>
2025-02-05 08:18:17 -0800

    Create Built-In-Functions-code.py

---
+++ Built-In-Functions-code.py
@@ -0,0 +1,16 @@
+print(ord("o"))
+print(ord("k"))
+
+print(chr(101))
+print(chr(72))
+
+myList = [123, 12,3, 1,34]
+print(sum(myList))
+print(len(myList))
+print(max(myList))
+print(min(myList))
+
+myNum = -123.45
+print(bin(myNum))
+print(round(myNum))
+print(abs(myNum))
