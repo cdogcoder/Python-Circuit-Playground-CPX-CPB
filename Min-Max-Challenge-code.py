f0080e1685d6c6bf021811387ec894ece016d97d
cdogcoder <cdogcoder@gmail.com>
2025-02-05 08:51:20 -0800

    Create Min-Max-Challenge-code.py

---
+++ Min-Max-Challenge-code.py
@@ -0,0 +1,17 @@
+my_string = 'STEAM Is Cool'
+print('The variable "my_string" = ',my_string)
+biggest = max(my_string)
+print('The biggest character in the \nstring "STEAM Is Cool" is',biggest)
+smallest = min(my_string)
+print('The smallest character in the \nstring "STEAM Is Cool" is',smallest)
+print('Done with Challenge 1')
+
+# the character "s" is higher in ASCII value than the character "S", which is why it is recognized as the max of the string
+
+user_string = input("enter a string: ")
+print('The variable "my_string" = ',my_string)
+biggest = max(my_string)
+print(f'The biggest character in the \nstring "{user_string}" is',biggest)
+smallest = min(my_string)
+print('The smallest character in the \nstring "STEAM Is Cool" is',smallest)
+print('Done with Challenge 2')
