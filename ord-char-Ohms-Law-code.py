f47dbeee0c0b388b1230d9845cbc9160dabc3d50
cdogcoder <cdogcoder@gmail.com>
2025-02-11 08:16:16 -0800

    Update ord-char-Ohms-Law-code.py

--- ord-char-Ohms-Law-code.py
+++ ord-char-Ohms-Law-code.py
@@ -1,6 +1,6 @@
 ohms_symbol = chr(ord("Ω"))
-voltage = 5
-current_in_milliamps = 20
+voltage = float(input("Enter a voltage value: ")
+current_in_milliamps = float(input("Enter a current value (in milliamps): ")
 
 print("?" + ohms_symbol, "=", str(voltage) + "V", "/", str(current_in_milliamps) + "mA")
 
@@ -8,4 +8,4 @@ current_in_amps = current_in_milliamps/1000
 answer = voltage/current_in_amps
 
 print(str(answer) + ohms_symbol, "=", str(voltage) + "V", "/", str(current_in_amps) + "A")
-
+print("A circuit with a voltage of", str(voltage)+"V", "and a current of", str(current_in_milliamps)+"mA", "will require a resistor of", str(answer)+ohms_symbol)
