971df4084ccec20350d3858984db59d3dae825ad
cdogcoder <cdogcoder@gmail.com>
2025-04-01 10:13:14 -0700

    Create file-lab-1.py

---
+++ file-lab-1.py
@@ -0,0 +1,7 @@
+fhand = open('text-file-matrix.txt', 'r')
+print(fhand)
+
+# _io.TextIOWrapper represents a buffered text stream
+# name='text-file-matrix.txt' is name of the file that is being opened ('text-file-matrix.txt')
+# mode='r' is the mode in which the file is being opened (read-only mode)
+# encoding='cp1252' specifies the encoding algorithm used to encode the contents of the file (CP-1252's legacy single-byte character encoding)
