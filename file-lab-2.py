d32fb443715ac9b90772c112d82cdabec3c9b8ff
cdogcoder <cdogcoder@gmail.com>
2025-04-02 08:22:33 -0700

    Create file-lab-2.py

---
+++ file-lab-2.py
@@ -0,0 +1,4 @@
+fhand = open('text-file-matrix2.txt', 'r')
+print(fhand)
+
+# Trying to open a file that does not exist returns a FileNotFoundError that states no such file or directory was found under that name
